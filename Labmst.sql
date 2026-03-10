3aa4b0c9a6bc1544ae4f7932a0768f3c64c34d69
Riya8216 <kasariya24@gmail.com>
2026-03-10 12:04:46 +0530

    Add files via upload

---
+++ Labmst.sql
@@ -0,0 +1,56 @@
+-- Uid-25MCA20033
+-- Ques 1.
+CREATE TABLE Purchases (
+    purchase_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    purchase_date DATE
+);
+
+INSERT INTO Purchases (customer_id, product_id, purchase_date) VALUES
+(101, 1, '2026-03-01'),
+(102, 1, '2026-03-01'),
+(103, 2, '2026-03-01'),
+(104, 1, '2026-03-01'),
+(105, 3, '2026-03-02'),
+(101, 2, '2026-03-02'),
+(102, 2, '2026-03-02'),
+(106, 1, '2026-03-01'),
+(107, 3, '2026-03-02'),
+(108, 1, '2026-03-03');
+
+SELECT Distinct
+    p1.customer_id AS customer_a,
+    p2.customer_id AS customer_b
+FROM Purchases p1
+JOIN Purchases p2
+ON p1.product_id = p2.product_id
+AND p1.purchase_date = p2.purchase_date
+AND p1.customer_id < p2.customer_id;
+
+-- Ques 2.
+CREATE TABLE Employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    manager_id INT,
+    department VARCHAR(50),
+    salary INT
+);
+INSERT INTO Employees VALUES
+(1, 'Amit', NULL, 'Management', 120000),
+(2, 'Ravi', 1, 'Engineering', 39000),
+(3, 'Neha', 1, 'Engineering', 82000),
+(4, 'Karan', 2, 'Engineering', 20000),
+(5, 'Simran', 2, 'Engineering', 62000),
+(6, 'Pooja', 3, 'Engineering', 41000),
+(7, 'Rahul', 3, 'Engineering', 64000),
+(8, 'Arjun', 1, 'HR', 10000);
+
+
+create view HighSalaryEmployees AS
+Select emp_id,emp_name,salary
+From Employees
+where salary>50000;
+
+
+Select * from HighSalaryEmployees;
